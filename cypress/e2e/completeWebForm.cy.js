f70446d495f2481fb57a648c4fe9e00b775d0852
Brunaplg <114033566+Brunaplg@users.noreply.github.com>
2025-02-26 18:58:00 -0500

    initial commit

---
+++ cypress/e2e/completeWebForm.cy.js
@@ -0,0 +1,26 @@
+
+describe('autocomplete the web form and submit', () => {
+  beforeEach(() => {
+    cy.visit('/form');
+  });
+
+  it('Fills out the form and submit.', () => {
+    cy.get('#first-name').type('Bruna');
+    cy.get('#last-name').type('Pelege');
+    cy.get('#job-title').type('QA Analyst');
+    cy.get('#radio-button-2').check();
+    cy.get('#checkbox-2').check();
+    cy.get('#select-menu').select('2');
+    cy.get('#datepicker').type('2025-03-01');
+    cy.get('a.btn.btn-lg.btn-primary').click();
+
+    // Assert navigation to the Thank You page
+    cy.url().should('include', '/thanks');
+
+    // Assert the heading text is visible
+    cy.get('h1').should('have.text', 'Thanks for submitting your form');
+
+    // Assert the heading text is visible
+    cy.get('h1').should('have.text', 'Thanks for submitting your form');
+  })
+})
